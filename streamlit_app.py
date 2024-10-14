48f6e10e372a19eb9b157de707d88c7f08ad66db
Aruna Giri <112208016+giriaruna@users.noreply.github.com>
2024-10-14 14:36:28 +0000

    Update

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,13 +9,14 @@ import streamlit.components.v1 as components
 
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from sklearn import metrics
+from sklearn import metrics as mt
 
 
 st.title("Project Happiness")
 
 app_page = st.sidebar.selectbox("Select Page", ['Data Exploration', 'Visualization', 'Prediction'])
 df = pd.read_csv("whr2023.csv")
+
 
 
 if app_page == "Data Exploration":
@@ -33,11 +34,9 @@ if app_page == "Data Exploration":
 
 if app_page == "Visualization":
     st.subheader("03. Data Visualization")
-
     list_columns = df.columns
 
     values = st.multiselect("Select two variables: ", list_columns, ["Happiness score", "Healthy life expectancy"])
-
     #Creation of the line chart
     st.line_chart(df, x=values[0], y=values[1])
 
@@ -54,20 +53,25 @@ if app_page == "Visualization":
     st.pyplot(pair)
 
 
-if app_page == "Prediction":
+if app_page == 'Prediction':
     st.title("03. Prediction")
         
     list_columns = df.columns
 
     input_lr = st.multiselect("Select two variables: ", list_columns, ["Happiness score", "Logged GDP per capita"])
 
-    df_new = df.dropna() # initially I was just droping the null val in the y and not in x so it was giving me valueerror
+    df_new = df.dropna() 
     df2 = df_new[input_lr]
 
     X = df2
     y = df_new["Healthy life expectancy"]
 
-    
+    col1,col2 = st.columns(2)
+    col1.subheader("Feature Columns top 25 ")
+    col1.write(X.head(25))
+    col2.subheader("Target Column top 25")
+    col2.write(y.head(25))
+
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state=42)
 
     lr = LinearRegression()
@@ -77,12 +81,10 @@ if app_page == "Prediction":
 
     lr.fit(X_train, y_train)
 
-    #Step 6 Evaluation
-    mae = metrics.mean_absolute_error(pred, y_test) # how far are we from the truth
-    r2 = metrics.r2_score(pred, y_test) # percentage how well you are improving
-
-    st.write("Mean Absolute Error:", mae)
-    st.write("R2:", r2)
+    st.subheader('🎯 Results')
 
 
-
+    st.write("1) The model explains,", np.round(mt.explained_variance_score(y_test, pred)*100,2),"% variance of the target feature")
+    st.write("2) The Mean Absolute Error of model is:", np.round(mt.mean_absolute_error(y_test, pred ),2))
+    st.write("3) MSE: ", np.round(mt.mean_squared_error(y_test, pred),2))
+    st.write("4) The R-Square score of the model is " , np.round(mt.r2_score(y_test, pred),2))
